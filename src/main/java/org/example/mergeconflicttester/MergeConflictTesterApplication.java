b5dfc5efea433b290dbe2227684d937791b7c07c
L-Jayawardhana <lakdinurameshjayawardhana@gmail.com>
2025-08-20 22:59:04 +0530

    first commit

---
+++ src/main/java/org/example/mergeconflicttester/MergeConflictTesterApplication.java
@@ -0,0 +1,13 @@
+package org.example.mergeconflicttester;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MergeConflictTesterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MergeConflictTesterApplication.class, args);
+    }
+
+}
